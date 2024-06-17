71230a238abde05dce54a7d1002dc4aacf3cfb6b
will-hd <107655543+will-hd@users.noreply.github.com>
2024-06-17 11:30:07 +0100

    Added descs

--- data/tempdata.py
+++ data/tempdata.py
@@ -12,12 +12,18 @@ class TempData:
     def __init__(self, data: pd.DataFrame, max_num_context: int):
         self.data = data
         self.max_num_context = max_num_context
-        x_values = data.iloc[0][1:].values.astype('float32')
+        x_values = data.iloc[0][3:].values.astype('float32')
+        print(x_values)
         self.x_values = torch.from_numpy(x_values).unsqueeze(0)  # Shape: [1, num_points]
-        self.y_values_train = torch.tensor(data.iloc[1:508, 1:].values).float()  # Shape: [num_samples, num_points]
-        self.y_values_val = torch.tensor(data.iloc[619:, 1:].values).float()  # Shape: [num_samples, num_points]
+        self.y_values_train = torch.tensor(data.iloc[1:508, 3:].values).float()  # Shape: [num_samples, num_points]
+        self.y_values_val = torch.tensor(data.iloc[619:, 3:].values).float()  # Shape: [num_samples, num_points]
+        self.y_desc = data.iloc[1:508, 2].values
 
-    def generate_batch(self, batch_size: int, device: torch.device = torch.device('cpu')) -> NPRegressionDescription:
+    def generate_batch(self, 
+                       batch_size: int, 
+                       device: torch.device = torch.device('cpu'),
+                       return_knowledge: bool = False
+                       ) -> NPRegressionDescription:
         num_total_points = self.x_values.size(-1)
         num_context = np.random.randint(low=3, high=self.max_num_context)
         num_target = num_total_points  # Using all points as target
@@ -35,14 +41,27 @@ class TempData:
         x_target = self.x_values.repeat(batch_size, 1)  # Shape: [batch_size, num_target]
         y_target = selected_y_values  # Shape: [batch_size, num_target]
 
-        return NPRegressionDescription(
-            x_context=x_context.unsqueeze(-1).to(device),  # Shape: [batch_size, num_context, x_size]
-            y_context=y_context.unsqueeze(-1).to(device),  # Shape: [batch_size, num_context, y_size]
-            x_target=x_target.unsqueeze(-1).to(device),    # Shape: [batch_size, num_target, x_size]
-            y_target=y_target.unsqueeze(-1).to(device),    # Shape: [batch_size, num_target, y_size]
-            num_total_points=num_total_points,
-            num_context_points=num_context
-        )
+        if return_knowledge:
+            knowledge = self.y_desc[selected_indices]
+            return NPRegressionDescription(
+                x_context=x_context.unsqueeze(-1).to(device),  # Shape: [batch_size, num_context, x_size]
+                y_context=y_context.unsqueeze(-1).to(device),  # Shape: [batch_size, num_context, y_size]
+                x_target=x_target.unsqueeze(-1).to(device),    # Shape: [batch_size, num_target, x_size]
+                y_target=y_target.unsqueeze(-1).to(device),    # Shape: [batch_size, num_target, y_size]
+                num_total_points=num_total_points,
+                num_context_points=num_context
+            ), knowledge
+
+        else:
+            
+            return NPRegressionDescription(
+                x_context=x_context.unsqueeze(-1).to(device),  # Shape: [batch_size, num_context, x_size]
+                y_context=y_context.unsqueeze(-1).to(device),  # Shape: [batch_size, num_context, y_size]
+                x_target=x_target.unsqueeze(-1).to(device),    # Shape: [batch_size, num_target, x_size]
+                y_target=y_target.unsqueeze(-1).to(device),    # Shape: [batch_size, num_target, y_size]
+                num_total_points=num_total_points,
+                num_context_points=num_context
+            )
  
 
 # # Load the data
