87cc258777309a60f8f246a8dbcbe9090c9a66ae
will-hd <107655543+will-hd@users.noreply.github.com>
2024-07-20 16:38:11 +0000

    Running description knowledge exps + improve plot function
    temp_II desc not seeming to train ...

--- src/plot.py
+++ src/plot.py
@@ -5,41 +5,43 @@ import numpy as np
 import logging
 logger = logging.getLogger(__name__)
 
-def plot_predictive(model, batch, save=False, iter=None):
+LINE_COLOURS = ['#1f77b4', '#ff7f0e', '#2ca02c',]
+FILL_COLOURS = ['#A6CEE3', '#f0c39c', '#b8deb8']
+
+def plot_predictive(model, batch, figsize=(5, 3), save=False, iter=None):
     '''
     Plot predicted mean and variance given context and targets. 
     '''
-
+    batch_size, _, _ = batch.x_context.shape
+    assert batch_size <= 3, 'Batch size should be <= 3 for plot clarity'
     with torch.no_grad():
         # make device be the device of the model
         p_y_pred, q_z_context, q_z_target = model(batch.x_context, batch.y_context, batch.x_target, batch.y_target, batch.knowledge)
         mu = p_y_pred.mean  # Shape [num_z_samples, batch_size, num_target_points, y_dim=1]
         sigma = p_y_pred.stddev
 
-    x_context = batch.x_context.cpu()
-    y_context = batch.y_context.cpu()
-    x_target = batch.x_target.cpu()
-    y_target = batch.y_target.cpu()
-    mu = mu.cpu()
-    sigma = sigma.cpu()
-    plt.figure(figsize=(5, 3))
-
-    plt.plot(x_target.flatten(), y_target.flatten(), 'k:')  # Plot ground truth GP
-    plt.scatter(x_context.flatten(), y_context.flatten(), c='k')  # Plot context points
-
-    num_z_samples = mu.shape[0]
-    indices_to_plot = np.random.choice(num_z_samples, size=3)
-    for i in indices_to_plot:
-        plt.plot(x_target.flatten(), mu[i].flatten())
-        plt.fill_between(
-            x_target.flatten(),
-            mu[i].flatten() - sigma[i].flatten(),
-            mu[i].flatten() + sigma[i].flatten(),
-            alpha=0.3,
-            facecolor='#A6CEE3',
-            interpolate=True)
+    x_context, y_context = batch.x_context.cpu(), batch.y_context.cpu()
+    x_target, y_target = batch.x_target.cpu(), batch.y_target.cpu()
+    mu, sigma = mu.cpu(), sigma.cpu()
+    
+    plt.figure(figsize=figsize)
+    for i in range(batch_size):
+        plt.plot(x_target[i].flatten(), y_target[i].flatten(), 'k:')  # Plot ground truth GP
+        plt.scatter(x_context[i].flatten(), y_context[i].flatten(), c='k')  # Plot context points
+    
+        num_z_samples = mu.shape[0]
+        z_sample_idx = np.random.choice(num_z_samples, size=3)
+        for j in z_sample_idx:
+            plt.plot(x_target[i].flatten(), mu[j, i].flatten(), color=LINE_COLOURS[i])
+            plt.fill_between(
+                x_target[i].flatten(),
+                mu[j, i].flatten() - sigma[j, i].flatten(),
+                mu[j, i].flatten() + sigma[j, i].flatten(),
+                alpha=0.3,
+                facecolor=FILL_COLOURS[i],
+                interpolate=True)
     #plt.ylim(-4, 4)
-    #plt.xlim(-2, 2)
+    plt.xlim(-2, 2)
     if save:
         plt.savefig(f'./results/iter_{iter}.png')
     plt.show()
